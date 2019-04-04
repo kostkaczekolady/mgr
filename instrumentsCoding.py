90ddc90c20c708a57cc6a5fd931a7c86b5c9b124
Dominika <nika.dziubinska@gmail.com>
2019-04-04 12:35:53 +0200

    add 10 cross

--- instrumentsCoding.py
+++ instrumentsCoding.py
@@ -11,34 +11,35 @@ for f in onlyfiles:
     trimmed_name = re.sub("_\d+.mp3", "", re.sub("_\d+_", "+", f))
     # print("zmieniona nazwa", trimmed_name)
     if trimmed_name == 'guitar':
-        data.append([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        data.append([1])
     elif trimmed_name == 'viola':
-        data.append([0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        data.append([2])
     elif trimmed_name == 'cello':
-        data.append([0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        data.append([3])
     elif trimmed_name == 'clarinet':
-        data.append([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        data.append([4])
     elif trimmed_name == 'cello+clarinet':
-        data.append([0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        data.append([5])
     elif trimmed_name == 'cello+guitar':
-        data.append([0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+        data.append([6])
     elif trimmed_name == 'cello+viola':
-        data.append([0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
+        data.append([7])
     elif trimmed_name == 'clarinet+guitar':
-        data.append([0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0])
+        data.append([8])
     elif trimmed_name == 'clarinet+viola':
-        data.append([0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0])
+        data.append([9])
     elif trimmed_name == 'guitar+viola':
-        data.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0])
+        data.append([10])
     elif trimmed_name == 'guitar+clarinet':
-        data.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
+        data.append([11])
     elif trimmed_name == 'cello+clarinet+triangle':
-        data.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0])
+        data.append([12])
     elif trimmed_name == 'cello+viola+triangle':
-        data.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0])
+        data.append([13])
     elif trimmed_name == 'clarinet+guitar+triangle':
-        data.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+        data.append([14])
     else:
-        data.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        data.append([0])
+        print("Dziwny instr: ", trimmed_name)
 
 # print("zakodowane elementy bazy:", data)
